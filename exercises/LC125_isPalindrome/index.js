724203d62b5d146d59491a09db231696eb13143b
Shahmir Khan Jadoon <shahmirkj@gmail.com>
2022-07-13 12:14:34 +0500

    Added solutions for leetcode problems 3,5,11,20,49,125,271,238 and 242

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,24 @@
-function isPalindrome(s) {}
+// Following code has:
+// Time complexity: O(n)
+// Space complexity: O(1) // left and right pointer takes up constant space
+
+function isPalindrome(s) {
+	let str = s.toLowerCase().replace(/[\W_]/g, ''); // remove special characters and spaces
+
+	let left = 0;
+	let right = str.length - 1;
+
+	while (left < right) {
+		let leftChar = str[left];
+		let rightChar = str[right];
+
+		if (leftChar !== rightChar) return false;
+
+		left++;
+		right--;
+	}
+
+	return true;
+}
 
 module.exports = isPalindrome;
--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,20 @@
-function groupAnagrams(strs) {}
+// Following code has:
+// Time complexity: O(n k log k) // n is the number of strings and k is the length of the strings
+// Space complexity: O(nk) // Data stored in grouped hashMap/hashTable
+
+const groupAnagrams = strs => {
+	let grouped = {};
+
+	for (let i = 0; i < strs.length; i++) {
+		const word = strs[i];
+		const key = word.split('').sort().join('');
+
+		if (!grouped[key]) grouped[key] = [];
+
+		grouped[key].push(word);
+	}
+
+	return Object.values(grouped);
+};
 
 module.exports = groupAnagrams;
